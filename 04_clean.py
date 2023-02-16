9dc31e0691bbba87fccee8efe83448b0849629cf
chattykathy1 <124712524+chattykathy1@users.noreply.github.com>
2023-02-16 15:26:23 -0500

    Initial commit

---
+++ 04_clean.py
@@ -0,0 +1,18 @@
+# remove punctuation/numbers/one character words
+import json
+
+def store_json(data,file):
+    # Enter your code here
+    pass
+
+with open('03_titles.json', 'r') as file:
+    # Enter your code here - load JSON
+
+    # remove punctuation/numbers
+    # Enter your code here
+
+    # remove one character words
+    # Enter your code here
+
+    # store_json(titles, '04_titles_clean.json')
+    pass
---
+++ 05_frequency.py
@@ -0,0 +1,19 @@
+#  count words
+# ---------------
+import json    
+
+def store_json(data,file):
+    # Enter your code here
+    pass
+
+with open('04_titles_clean.json', 'r') as file:
+    # Enter your code here - load json
+
+    # remove punctuation/numbers
+    # Enter your code here
+
+    # count words
+    # Enter your code here
+
+    # store_json(counts, 'visualization/word_count.js')
+    pass
